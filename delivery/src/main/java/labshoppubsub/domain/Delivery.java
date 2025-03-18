a8007b07bba0924eecefb6477852a5846008e813
Sunki Lee <sun95132@naver.com>
2025-03-18 01:21:42 +0000

    change

--- delivery/src/main/java/labshoppubsub/domain/Delivery.java
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -43,24 +43,10 @@ public class Delivery {
     public static void addDelivery(OrderPlaced orderPlaced) {
         //implement business logic here:
 
-        /** Example 1:  new item 
+        // Example 1:  new item 
         Delivery delivery = new Delivery();
         repository().save(delivery);
-
-        */
-
-        /** Example 2:  finding and process
-        
-
-        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
-            
-            delivery // do something
-            repository().save(delivery);
-
-
-         });
-        */
-
+    
     }
     //>>> Clean Arch / Port Method
 
